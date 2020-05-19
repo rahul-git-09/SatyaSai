7624e8d1c9add3f5608e5a539801b4b167dea7e5
rahul-git-09 <rahulsays09@gmail.com>
2020-05-19 14:19:11 +0530

    statifiles rooute modified

--- School/settings.py
+++ School/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static','../blog/static','../user/static'),
+    os.path.join(BASE_DIR, 'media')
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
